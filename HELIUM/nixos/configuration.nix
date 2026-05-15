a65d2b78d4d706227727b944ca7cf4810e706913
Vadim Averin <vadim@avevad.com>
2026-05-16 02:26:10 +0300

    Network configuration of HELIUM

--- HELIUM/nixos/configuration.nix
+++ HELIUM/nixos/configuration.nix
@@ -5,9 +5,37 @@
     /etc/nixos/hardware-configuration.nix
   ];
 
-  networking.hostName = "HELIUM";
-  networking.firewall.enable = false;
-  networking.networkmanager.enable = true; # TODO: replace with static config
+  networking = {
+    useDHCP = false;
+    firewall.enable = false;
+
+    hostName = "HELIUM";
+    fqdn = "helium.avevad.com";
+
+    domain = "avedus.pro";
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    interfaces.ens3.ipv4 = {
+      addresses = [ { address = "146.103.124.240"; prefixLength = 24; } ];
+      routes = [
+        { address = "0.0.0.0"; prefixLength = 0; via = "146.103.124.1"; }
+        { address = "146.103.124.0"; prefixLength = 24; }
+      ];
+    };
+
+    wireguard.interfaces = {
+      wg1 = {
+        listenPort = 51340;
+        ips = [ "10.254.254.1/30" ];
+        privateKeyFile = "/var/wireguard/key1.txt";
+        peers = [
+          { # NITROGEN
+            publicKey = "frYUUl/wWzMUeiIzjjzZeAkWCg7tie4KwtCK3yqCum8=";
+            allowedIPs = [ "10.254.254.2" ];
+          }
+        ];
+      };
+    };
+  };
 
   time.timeZone = "Europe/Amsterdam";
 
